1959fee51b86bb339efab874e0b65afee41c7b8d
QuentinVilfeu <quentin.vilfeu.dev@gmail.com>
2024-08-05 15:35:08 +0200

    mise en place bouton avec props/children

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Button from '../ui/Button/Button'
+
+function App() {
+  return (
+    <div className="App">
+      <Button text="Valider" />
+      <Button text="Annuler" />
+      <Button>Valider</Button>
+      <Button>Annuler</Button>
+      <Button><img src="/3857-78531.png" alt="" /></Button>
+    </div>
+    
+  )
+}
+
+export default App
---
+++ src/components/ui/Button/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import style from './Button.module.css'
+
+const Button = (props) => {
+    console.log(props);
+    return (
+        <div className={style.Button} data-testid="Button">{props.children}</div>
+    )
+};
+
+export default Button;
